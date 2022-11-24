97dbf2552ad658f273c0ad57cded8c4976dd82e1
Luigi213 <reyesgianluigi88@gmail.com>
2022-11-24 13:34:36 +0100

    Add the prompt

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// -1-2-3 inserimento dei prompt: nome, cognome e colore preferito 
+let nome = prompt("Inserisci il nome")
+let cognome = prompt("Inserisci il cognome")
+let colore_preferito = prompt("Inserisci il colore preferito")
+// - concatenizzazione dei valori delle variabili 
