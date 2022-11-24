69aa892aeafcd9f408c9b15c4a228eea12bbbfd1
Luigi213 <reyesgianluigi88@gmail.com>
2022-11-24 13:40:57 +0100

    completed

--- js/script.js
+++ js/script.js
@@ -2,4 +2,6 @@
 let nome = prompt("Inserisci il nome")
 let cognome = prompt("Inserisci il cognome")
 let colore_preferito = prompt("Inserisci il colore preferito")
-// - concatenizzazione dei valori delle variabili 
+// -4-5 concatenizzazione dei valori delle variabili con in fine il numero 21
+let number = 21
+document.getElementById("date").innerHTML = `${nome} ${cognome} ${colore_preferito} ${number}`
